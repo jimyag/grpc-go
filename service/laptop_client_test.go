cf1c1f4469d202d6d45b7f091f7e60b1ca092485
jimyag <jimyag@126.com>
2022-02-15 20:48:13 +0800

    test：search laptop test

--- service/laptop_client_test.go
+++ service/laptop_client_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/jimyag/grpc-go/service"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
+	"io"
 	"net"
 	"testing"
 )
@@ -21,7 +22,7 @@ func TestClientCreateLaptop(t *testing.T) {
 	// 测试并行执行
 	t.Parallel()
 
-	laptopServer, serverAddress := startTestLaptopServer(t)
+	laptopServer, serverAddress := startTestLaptopServer(t, service.NewInMemoryLaptopStore())
 	laptopClient := newClientLaptopClient(t, serverAddress)
 
 	laptop := sample.NewLaptop()
@@ -43,14 +44,108 @@ func TestClientCreateLaptop(t *testing.T) {
 }
 
 //
+// TestLaptopServer_SearchLaptop
+//  @Description: 测试有两种方法，第一种是 mock pb/laptop_service.pb.go 中 LaptopService_SearchLaptopServer 的流接口。
+// 				还需要添加此接口的实现
+//	type ServerStream interface {
+//    SetHeader(metadata.MD) error
+//    SendHeader(metadata.MD) error
+//    SetTrailer(metadata.MD)
+//    Context() context.Context
+//    SendMsg(m interface{}) error
+//    RecvMsg(m interface{}) error
+//}
+//	第二种方法 使用客户端调用测试服务器的RPC
+//  @param t
+//
+func TestLaptopServer_SearchLaptop(t *testing.T) {
+	t.Parallel()
+
+	filter := &pb.Filter{
+		MaxPriceUsd: 2000,
+		MinCpuCores: 4,
+		MinCpuGhz:   2.2,
+		MinRam: &pb.Memory{
+			Value: 8,
+			Unit:  pb.Memory_GB,
+		},
+	}
+
+	store := service.NewInMemoryLaptopStore()
+	expectedIDs := make(map[string]bool)
+
+	laptopCount := 6
+	for i := 0; i < laptopCount; i++ {
+		laptop := sample.NewLaptop()
+		switch i {
+		case 0:
+			laptop.PriceUsd = 2500
+		case 1:
+			laptop.Cpu.NumberCores = 2
+		case 2:
+			laptop.Cpu.MinGhz = 2.0
+		case 3:
+			laptop.Memory = &pb.Memory{
+				Value: 4096,
+				Unit:  pb.Memory_MB,
+			}
+		case 4:
+			laptop.PriceUsd = 1999
+			laptop.Cpu.NumberThreads = 4
+			laptop.Cpu.MinGhz = 2.5
+			laptop.Cpu.MaxGhz = 4.5
+			laptop.Memory = &pb.Memory{
+				Value: 16,
+				Unit:  pb.Memory_GB,
+			}
+			expectedIDs[laptop.Id] = true
+		case 5:
+			laptop.PriceUsd = 2000
+			laptop.Cpu.NumberThreads = 5
+			laptop.Cpu.MinGhz = 2.7
+			laptop.Cpu.MaxGhz = 4.9
+			laptop.Memory = &pb.Memory{
+				Value: 8,
+				Unit:  pb.Memory_GB,
+			}
+			expectedIDs[laptop.Id] = true
+
+		}
+		err := store.Save(laptop)
+		require.NoError(t, err)
+	}
+
+	_, serverAddress := startTestLaptopServer(t, store)
+	laptopClient := newClientLaptopClient(t, serverAddress)
+
+	req := &pb.SearchLaptopRequest{Filter: filter}
+	stream, err := laptopClient.SearchLaptop(context.Background(), req)
+	require.NoError(t, err)
+
+	found := 0
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		require.NoError(t, err)
+		require.Contains(t, expectedIDs, res.GetLaptop().GetId())
+		found += 1
+	}
+
+	require.Equal(t, len(expectedIDs), found)
+}
+
+//
 // startTestLaptopServer
 //  @Description:
 //  @param t
 //  @return *service.LaptopServer
 //  @return string
 //
-func startTestLaptopServer(t *testing.T) (*service.LaptopServer, string) {
-	laptopServer := service.NewLaptopServer(service.NewInMemoryLaptopStore())
+func startTestLaptopServer(t *testing.T, store service.LaptopStore) (*service.LaptopServer, string) {
+	laptopServer := service.NewLaptopServer(store)
 	grpcServer := grpc.NewServer()
 	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
 	// 0表示 任意一个可用的端口
